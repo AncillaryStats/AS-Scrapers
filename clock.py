96c947a3bc5660fa4d9ebd8013b4de46dba03300
Ansel Rosenberg <ansel01@gmail.com>
2015-10-03 23:04:17 -0700

    Fix sub.py, add Procfile, remove clock

--- clock.py
+++
@@ -1,9 +0,0 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
-
-sched = BlockingScheduler()
-
-@sched.scheduled_job('interval', seconds=5)
-def timed_job():
-    print('This job is run every five seconds.')
-
-sched.start()
--- sub.py
+++ sub.py
@@ -2,7 +2,7 @@ import redis
 import run
 import os
 
-redis_url = os.environ('REDISTOGO_URL')
+redis_url = os.getenv('REDISTOGO_URL')
 
 r = redis.StrictRedis.from_url(redis_url)
 channel = r.pubsub()
@@ -10,7 +10,8 @@ channel.subscribe('scrapers')
 
 while True:
     message = channel.get_message()
-    if message and message['data'] == 'CRAWL ALL SPIDERS':
-        print 'CRAWLING ALL SPIDERS'
-        run.crawl_all()
-        print 'DONE CRAWLING'
+    if message:
+        print message['data']
+        if message['data'] == 'CRAWL ALL SPIDERS':
+            run.crawl_all()
+            print 'DONE CRAWLING'
