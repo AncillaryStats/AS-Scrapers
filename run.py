d2ff96b1d96b4cd1971d94386f156b163c43a55c
Ansel Rosenberg <ansel01@gmail.com>
2015-10-04 19:27:03 -0700

    Update run.py to allow command to only crawl player games

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ def crawl_spider(spider_name):
     Writes to 'espn_scraper/track_crawlers.txt' before starting crawl
     """
 
-    f = open('track_crawlers', 'a')
+    f = open('track_crawlers.txt', 'a')
     f.write('crawling spider %s @ %s\n' % (spider_name, datetime.datetime.now()))
     f.close()
     os.system('scrapy crawl %s' % spider_name)
@@ -29,10 +29,27 @@ def crawl_all():
     f.write('DONE CRAWLING @ %s\n' % datetime.datetime.now())
     f.close()
 
+def crawl_games():
+    """Runs all game stats spiders"""
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    print 'current dir %s' % cwd
+    os.chdir(cwd + '/nfl/')
+    new_dir = os.path.dirname(os.path.realpath(__file__))
+    print 'new dir %s '% new_dir
+
+    spiders = ['nfl_qb_stats', 'nfl_rb_stats', 'nfl_wr_stats', 'nfl_te_stats']
+
+    for spider in spiders:
+       crawl_spider(spider)
+
+    f = open('track_crawlers.txt', 'a')
+    f.write('DONE CRAWLING @ %s\n' % datetime.datetime.now())
+    f.close()
+
 def print_dirs():
     """Test directory changing"""
     cwd = os.path.dirname(os.path.realpath(__file__))
     print 'current dir %s' % cwd
     os.chdir(cwd + '/nfl/')
-    new_dir = os.path.dirname(os.path.realpath(__file__))
+    new_dir = os.path.dirngsame(os.path.realpath(__file__))
     print 'new dir %s '% new_dir
